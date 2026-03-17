7a1539a9287be4411730ae5942babf3ea473622f
justinleveille2005 <justin.leveille2005@gmail.com>
2026-03-17 13:05:31 -0400

    Update numero_1.py

--- numero_1.py
+++ numero_1.py
@@ -8,24 +8,24 @@ Justin Léveillé
 Benjamin Grégoire
 Matis Veilleux
 """
-
 import math
 
-def calcul_billot(L_TOT_BILLOT,L_SUBMERGEE_BILLOT,D_BILLOT,RHO_EAU,PRESSION):
-    
-    CENTRE_FLOTTAISON=L_SUBMERGEE_BILLOT/2
-    VOL_SUB=L_SUBMERGEE_BILLOT*(D_BILLOT/2)**2*math.pi
-    VOL_TOT=L_TOT_BILLOT*(D_BILLOT/2)**2*math.pi
-    FB=RHO_EAU*9.81*VOL_SUB
-    RHO_BILLOT = (CENTRE_FLOTTAISON*FB)/((L_TOT_BILLOT/2)*9.81*VOL_TOT)
-    POIDS_SPEC_BILLOT = RHO_BILLOT*9.81
-    W = RHO_BILLOT*9.81*VOL_TOT
-    TENSION_CORDE = FB-W
-    H = PRESSION/(RHO_EAU*9.81)
-    angle = (180/math.pi)*math.asin(H/L_SUBMERGEE_BILLOT)
-    
+def calcul_billot(l_tot_billot,l_submerge_billot,d_billot,rho_eau,pression):
+
+    centre_flottaison=l_submerge_billot/2
+    vol_sub=l_submerge_billot*(d_billot/2)**2*math.pi
+    vol_tot=l_tot_billot*(d_billot/2)**2*math.pi
+    fb=rho_eau*9.81*vol_sub
+    rho_billot = (centre_flottaison*fb)/((l_tot_billot/2)*9.81*vol_tot)
+    poids_spec_billot = rho_billot*9.81
+    w = rho_billot*9.81*vol_tot
+    tension_corde = fb-w
+    h = pression/(rho_eau*9.81)
+    angle = (180/math.pi)*math.asin(h/l_submerge_billot)
+
     print(angle)
-    print(TENSION_CORDE)
-    print(POIDS_SPEC_BILLOT)
+    print(tension_corde)
+    print(poids_spec_billot)
+
 
 
