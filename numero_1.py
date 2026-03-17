56b70ca4223b741a39d2baa822814a403f06c397
justinleveille2005 <justin.leveille2005@gmail.com>
2026-03-17 12:48:32 -0400

    Update numero_1.py

--- numero_1.py
+++ numero_1.py
@@ -9,9 +9,23 @@ Benjamin Grégoire
 Matis Veilleux
 """
 
-import numpy as np
 import math
 
-def
+def calcul_billot(L_TOT_BILLOT,L_SUBMERGEE_BILLOT,D_BILLOT,RHO_EAU,PRESSION):
+    
+    CENTRE_FLOTTAISON=L_SUBMERGEE_BILLOT/2
+    VOL_SUB=L_SUBMERGEE_BILLOT*(D_BILLOT/2)**2*math.pi
+    VOL_TOT=L_TOT_BILLOT*(D_BILLOT/2)**2*math.pi
+    FB=RHO_EAU*9.81*VOL_SUB
+    RHO_BILLOT = (CENTRE_FLOTTAISON*FB)/((L_TOT_BILLOT/2)*9.81*VOL_TOT)
+    POIDS_SPEC_BILLOT = RHO_BILLOT*9.81
+    W = RHO_BILLOT*9.81*VOL_TOT
+    TENSION_CORDE = FB-W
+    H = PRESSION/(RHO_EAU*9.81)
+    angle = (180/math.pi)*math.asin(H/L_SUBMERGEE_BILLOT)
+    
+    print(angle)
+    print(TENSION_CORDE)
+    print(POIDS_SPEC_BILLOT)
 
 
