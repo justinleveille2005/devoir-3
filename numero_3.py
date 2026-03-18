01ba64feb53929e7bc2820553d8dee400604eba6
justinleveille2005 <justin.leveille2005@gmail.com>
2026-03-18 08:54:24 -0400

    Update numero_3.py

--- numero_3.py
+++ numero_3.py
@@ -8,6 +8,35 @@ Devoir 3 numéro 2
 
 Justin Léveillé
 Benjamin Grégoire
-Matis Veilleux5
+Matis Veilleux
 """
+import numpy as np
 
+def equation_colebrooke(diametre,debit,mv,rugosite,viscosite):
+    """
+    
+    Parameters
+    ----------
+    diametre : Entier ou décimal. Diamètre de la conduite en (m).Doit être 
+               supérieur à zéro
+        
+    debit : Entier ou décimal. Débit du fluide en (m/s).Doit être supérieur à
+            zéro
+        
+    mv : Entier ou décimal. Masse volumique du fluide en (kg/m^3) 
+        
+    rugosite : Décimal. Rugosité de la conduite en (m)
+        
+    viscosite : Décimal. Viscosité dynamique du fluide en (kg/m*s)
+        
+    Returns
+    -------
+    None.
+
+    """
+    
+    aire=(diametre**2*np.pi)/4
+    
+    vitesse=debit/aire
+    
+    reynolds=mv*vitesse*diametre/viscosite
